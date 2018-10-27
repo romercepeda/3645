133cf08a55ecb18a15c73b21737880eef05f489d
romercepeda <romercepeda@gmail.com>
2018-10-27 11:17:41 -0300

    ejercicio

---
+++ ProductoFinancieros/js/productosFinancieros.js
@@ -0,0 +1,12 @@
+var nombre = "Alejandra";
+var apellido = "Castro";
+var nroDocumento = "97876786";
+var tipoDocumento = "DNI"
+var telefono = "11337568747";
+var email = "ale@gmail.com";
+
+var productos = ["Tj: Visa", "Tj Master", "Caja de Ahorro", "Caja Corriente"];
+
+function MostraClienteYsusProductos() {
+    document.getElementById("lblNombre").innerText = nombre;
+}
